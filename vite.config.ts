719153338e21532afbc099c1ac0c2a0f076db956
borysinho <borysquiroga@gmail.com>
2025-11-10 22:52:17 -0400

    fix: vercel url

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:3001",
+        target: "https://server-frontend-backend-generator.vercel.app",
         changeOrigin: true,
         secure: false,
       },
