27ed305e7bb569a24ebe835539b249224694ef41
borysinho <borysquiroga@gmail.com>
2025-11-10 21:16:49 -0400

    server url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/client-frontend-backend-generator/',
+  base: "/client-frontend-backend-generator/",
   server: {
     host: "localhost",
     port: 5173,
